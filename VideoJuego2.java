abcc2128f25aed9cf50f3a5e4814cdbb6bbe6b93
DevBr4yan <bauccacusic@unsa.edu.pe>
2024-10-18 18:18:57 -0500

    Add files via upload

--- VideoJuego2.java
+++ VideoJuego2.java
@@ -1,5 +1,4 @@
 package L5;
-
 import java.util.*;
 public class VideoJuego2 {
     public static void main(String [] args){
@@ -35,30 +34,30 @@ public class VideoJuego2 {
         mostrarTabla(campo);
         mostrarMayorVida(campo);
         promedioVida(campo);
-        nivelDeVidaEjercito(ordenCreacion);
+        nivelDeVidaEjercito(campo);
         System.out.println("\nDatos de los soldados en orden de creación:");
-        mostrar(ordenCreacion);
-        aleatorio(ordenCreacion);
+        mostrar(campo);
+        aleatorio(campo);
         System.out.println("\nAleatorio");
-        mostrar(ordenCreacion);
+        mostrar(campo);
         System.out.println("\nOrdenamiento BURBUJA");
-        ordenarPorVidaBurbuja(ordenCreacion);
-        mostrar(ordenCreacion);
-        aleatorio(ordenCreacion);
+        ordenarPorVidaBurbuja(campo);
+        mostrar(campo);
+        aleatorio(campo);
         System.out.println("\nAleatorio");
 
-        mostrar(ordenCreacion);
+        mostrar(campo);
         System.out.println("\nOrdenamiento SELECCION");
-        ordenarPorVidaSeleccion(ordenCreacion);
-        mostrar(ordenCreacion);
+        ordenarPorVidaSeleccion(campo);
+        mostrar(campo);
         System.out.println("\nRanking de mayor a menor: ");
-        rankingMayorMenor(ordenCreacion);
-        mostrar(ordenCreacion);
+        rankingMayorMenor(campo);
+        mostrar(campo);
 
     }
 
 
-    public static void mostrarTabla(Soldado[][] camp) {
+    public static void mostrarTabla(Soldado[][] camp){
         for(char i = 'A'; i < 'K'; i++)
         {
             System.out.print("       " + i + "  ");
@@ -92,9 +91,12 @@ public class VideoJuego2 {
         Soldado mayor = null;
         int filMayor = 0;
         int colMayor = 0;
-        for (int i = 0; i < campo.length; i++) {
+        for (int i = 0; i < campo.length; i++){
             for(int j = 0; j < campo[i].length; j++){
+                // Verifica que la posición actual tenga un soldado y que esté activo
                 if (campo[i][j] != null && campo[i][j].getEstado()){
+                    // Si no hay soldado registrado o el actual tiene más vida que el registrado
+                    // Actualiza el soldado con mayor vida
                     if(mayor == null || campo[i][j].getVida() > mayor.getVida()){
                         mayor = campo[i][j];
                         filMayor = i;
@@ -111,9 +113,12 @@ public class VideoJuego2 {
 
     public static void promedioVida(Soldado[][] campo){
         int suma = 0;
+        // Recorre todo el campo de soldados
         for(int i = 0; i < campo.length; i++){
             for(int j = 0; j < campo[i].length; j++){
+                // Si la posición actual contiene un soldado, suma su vida al total
                 if(campo[i][j] != null){
+                    // Acumula el valor de la vida del soldado
                     suma += campo[i][j].getVida();
                 }
             }
@@ -121,64 +126,128 @@ public class VideoJuego2 {
         System.out.println("\nEl promedio de vida de los 10 soldados es: " + (suma / campo.length));
     }
 
-    public static void nivelDeVidaEjercito(Soldado[] ordenSoldados){
+    public static void nivelDeVidaEjercito(Soldado[][] campo){
         int suma = 0;
-        for(int i = 0; i < ordenSoldados.length; i++) {
-            suma += ordenSoldados[i].getVida();
+        for (int i = 0; i < campo.length; i++){
+            for (int j = 0; j < campo[i].length; j++){
+                if (campo[i][j] != null){
+                    suma += campo[i][j].getVida();
+                }
+            }
         }
-        System.out.println("\nEl nivel de vida del ejercito es: " + suma);
+        System.out.println("\nEl nivel de vida del ejército es: " + suma);
     }
+    
 
-    public static void mostrar(Soldado[] ordenSoldados){
+    public static void mostrar(Soldado[][] campo){
         System.out.println();
-        for (Soldado soldado : ordenSoldados) {
-            System.out.println(soldado.toString());
-        }
-    }
-
-
-    public static void ordenarPorVidaBurbuja(Soldado[] ordenSoldados){
-        for (int i = 0; i < ordenSoldados.length; i++) {
-            for (int j = 0; j < ordenSoldados.length - 1 - i; j++) {
-                if(ordenSoldados[j].getVida() > ordenSoldados[j + 1].getVida()) {
-                    Soldado temp = ordenSoldados[j];
-                    ordenSoldados[j] = ordenSoldados[j + 1];
-                    ordenSoldados[j + 1] = temp;
+        // Recorre todo el campo de soldados
+        for (int i = 0; i < campo.length; i++){
+            // Si hay un soldado en la posición actual, imprime su información
+            for (int j = 0; j < campo[i].length; j++){
+                if (campo[i][j] != null){
+                    // Muestra los datos del soldado
+                    System.out.println(campo[i][j].toString());
                 }
             }
         }
     }
-    public static void ordenarPorVidaSeleccion(Soldado[] ordenCreacion) {
-        for (int i = 0; i < ordenCreacion.length - 1; i++) {
-            int indexMenor = i;
-            for (int j = i + 1; j < ordenCreacion.length; j++) {
-                if (ordenCreacion[j].getVida() < ordenCreacion[indexMenor].getVida()) {
-                    indexMenor = j; 
+    
+
+
+    public static void ordenarPorVidaBurbuja(Soldado[][] campo) {
+        Soldado temp;
+        // Recorremos todo el array bidimensional
+        for (int i = 0; i < campo.length; i++) {
+            for (int j = 0; j < campo[i].length; j++) {
+                // Comparar el soldado actual con el resto de los soldados
+                for (int k = 0; k < campo.length; k++) {
+                    for (int l = 0; l < campo[k].length; l++) {
+                        // Comparar vida de soldados e intercambiar si están desordenados
+                        if (campo[i][j] != null && campo[k][l] != null) {
+                            if (campo[i][j].getVida() < campo[k][l].getVida()) {
+                                temp = campo[i][j];
+                                campo[i][j] = campo[k][l];
+                                campo[k][l] = temp;
+                            }
+                        }
+                    }
                 }
-            }
-            if (indexMenor != i) {
-                Soldado temp = ordenCreacion[i];
-                ordenCreacion[i] = ordenCreacion[indexMenor];
-                ordenCreacion[indexMenor] = temp;
             }
         }
     }
-    public static void aleatorio(Soldado[] ordenCreacion) {
+    
+    
+    public static void ordenarPorVidaSeleccion(Soldado[][] campo){
+        Soldado temp;
+        // Recorre todas las posiciones del campo
+        for (int i = 0; i < campo.length; i++){
+            for (int j = 0; j < campo[i].length; j++){
+                int filMenor = i;
+                int colMenor = j;
+                // Busca el soldado con la menor vida a partir de la posición actual
+                for (int k = i; k < campo.length; k++){
+                    for (int l = 0; l < campo[k].length; l++){
+                        // Evita revisar posiciones ya evaluadas
+                        if (k == i && l <= j) {
+                            continue;
+                        }
+                        // Cambia las coordenadas si se encuentra un soldado con vida más baja
+                        if (campo[k][l] != null && campo[filMenor][colMenor] != null){
+                            if (campo[k][l].getVida() < campo[filMenor][colMenor].getVida()){
+                                filMenor = k;
+                                colMenor = l;
+                            }
+                        }
+                    }
+                }
+                // Intercambia los soldados si se encontró uno con menor vida
+                if (filMenor != i || colMenor != j){
+                    temp = campo[i][j];
+                    campo[i][j] = campo[filMenor][colMenor];
+                    campo[filMenor][colMenor] = temp;
+                }
+            }
+        }
+    }
+    
+    
+    public static void aleatorio(Soldado[][] campo){
         Random rand = new Random();
-        for (int i = ordenCreacion.length - 1; i > 0; i--) {
-            int j = rand.nextInt(i + 1);
-            Soldado temp = ordenCreacion[i];
-            ordenCreacion[i] = ordenCreacion[j];
-            ordenCreacion[j] = temp;
+        Soldado temp;
+        // Recorre todo el campo de soldados
+        for (int i = 0; i < campo.length; i++){
+            for (int j = 0; j < campo[i].length; j++){
+                // Genera una fila y columna aleatoria dentro de la matriz
+                int randFila = rand.nextInt(campo.length);
+                int randColumna = rand.nextInt(campo[0].length);
+                 // Intercambia el soldado en (i, j) con uno en una posición aleatoria (randFila, randColumna)
+                temp = campo[i][j];
+                campo[i][j] = campo[randFila][randColumna];
+                campo[randFila][randColumna] = temp;
+            }
         }
     }
-    public static void rankingMayorMenor(Soldado[] ordenSoldados) {
-        for (int i = 0; i < ordenSoldados.length; i++) {
-            for (int j = 0; j < ordenSoldados.length - 1 - i; j++) {
-                if (ordenSoldados[j].getVida() < ordenSoldados[j + 1].getVida()) {
-                    Soldado temp = ordenSoldados[j];
-                    ordenSoldados[j] = ordenSoldados[j + 1];
-                    ordenSoldados[j + 1] = temp;
+    
+    public static void rankingMayorMenor(Soldado[][] campo){
+        Soldado temp;
+        // Recorre todo el campo de soldados
+        for (int i = 0; i < campo.length; i++){
+            for (int j = 0; j < campo[i].length; j++){
+                // Compara cada soldado con todos los demás en el campo
+                for (int k = 0; k < campo.length; k++){
+                    for (int l = 0; l < campo[k].length; l++){
+                        // Verifica que ambos soldados no sean nulos
+                        if (campo[i][j] != null && campo[k][l] != null){
+                            // Si el soldado en (i, j) tiene más vida que el soldado en 
+                            //(k, l), los intercambia
+                            if (campo[i][j].getVida() > campo[k][l].getVida()){
+                                temp = campo[i][j];
+                                campo[i][j] = campo[k][l];
+                                campo[k][l] = temp;
+                            }
+                        }
+                    }
                 }
             }
         }
