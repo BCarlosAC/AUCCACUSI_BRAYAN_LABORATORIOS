490b0ed4a671f66fb0f5ab74a2cb73ea85f69a4b
Brayan Carlos Auccacusi Conde <bauccacusic@unsa.edu.pe>
2024-11-15 14:31:43 -0500

    Implementacion metodos de ranking, aleatorio, determinar ganador y llamado a los metodos en el main

--- VideoJuego4.java
+++ VideoJuego4.java
@@ -11,7 +11,35 @@ public class VideoJuego4 {
         asignarSoldados(campo, ej1);
         asignarSoldados(campo, ej2);
         mostrarTabla(campo);
+        System.out.println("Mayor vida ejercito 1: \n" + ej1[mayorVida(ej1)]);
+        System.out.println("Mayor vida ejercito 2: \n" + ej2[mayorVida(ej2)]);
+        System.out.println("Promedio vida ejercito 1: " + promedioEjercito(ej1));
+        System.out.println("Promedio vida ejercito 2: " + promedioEjercito(ej1));
+        System.out.println("Datos en que fueron creados");
+        mostrar(ej1, 1);
+        mostrar(ej2, 2);
+        System.out.println("Ranking de poder mayor a menor vida");
+        System.out.println("BURBUJA");
+        rankingMayorMenorBurbuja(ej1);
+        mostrar(ej1, 1);
+        rankingMayorMenorBurbuja(ej2);
+        mostrar(ej2, 2);
+
+        System.out.println("Aleatorio");
+        aleatorio(ej1);
+        aleatorio(ej2);
+        mostrar(ej1, 1);
+        mostrar(ej2, 2);
+
+        System.out.println("SELECCION");
+        rankingMayorMenorSeleccion(ej1);
+        mostrar(ej1, 1);
+        rankingMayorMenorSeleccion(ej2);
+        mostrar(ej2, 2);
+
+        determinarGanador(ej1, ej2);
     }
+    
     public static void crearSoldados(Soldado[] ej, char a){
         Random rand = new Random();
         for(int i = 0; i < ej.length; i++){
@@ -39,6 +67,7 @@ public class VideoJuego4 {
                 }
             }
         }
+    
     }
 
 
@@ -77,7 +106,7 @@ public class VideoJuego4 {
 
     public static double promedioEjercito(Soldado[] ej){
         double sumLife = 0;
-        for(int i = 0; i < ej.length - 1; i++){
+        for(int i = 0; i < ej.length; i++){
             sumLife += ej[i].getVida();
         }
         return sumLife/ej.length;
@@ -97,147 +126,58 @@ public class VideoJuego4 {
         Soldado aux;
         int r1, r2;
         for(int i = 0; i < ej.length; i++){
-            r1 = rand.nextInt(10) + 1;
-            r2 = rand.nextInt(10) + 1;
+            r1 = rand.nextInt(ej.length);
+            r2 = rand.nextInt(ej.length);
             aux = ej[r1];
             ej[r1] = ej[r2];
             ej[r2] = aux;
         }
     }
-
     
-    public static void rankingMayorMenorBurbuja(ArrayList<ArrayList<Soldado>> campo) {
-        ArrayList<Soldado> ejercito1 = new ArrayList<>();
-        ArrayList<Soldado> ejercito2 = new ArrayList<>();
-    
-        // Dividir soldados en Ejército 1 y Ejército 2
-        for (int i = 0; i < campo.size(); i++) {
-            for (int j = 0; j < campo.get(i).size(); j++) {
-                Soldado soldado = campo.get(i).get(j);
-                if (soldado != null) {
-                    // Si el nombre termina en '1', pertenece al Ejército 1
-                    if (soldado.getNombre().charAt(soldado.getNombre().length() - 1) == '1') {
-                        ejercito1.add(soldado);
-                    }
-                    // Si el nombre termina en '2', pertenece al Ejército 2
-                    else if (soldado.getNombre().charAt(soldado.getNombre().length() - 1) == '2') {
-                        ejercito2.add(soldado);
-                    }
-                }
-            }
-        }
-    
-        // Ordenar Ejército 1 en orden descendente de vida (mayor a menor)
-        for (int i = 0; i < ejercito1.size() - 1; i++) {
-            for (int j = 0; j < ejercito1.size() - 1 - i; j++) {
-                if (ejercito1.get(j).getVida() < ejercito1.get(j + 1).getVida()) {
-                    // Intercambiar si está en el orden incorrecto
-                    Soldado temp = ejercito1.get(j);
-                    ejercito1.set(j, ejercito1.get(j + 1));
-                    ejercito1.set(j + 1, temp);
-                }
-            }
-        }
-    
-        // Ordenar Ejército 2 en orden descendente de vida (mayor a menor)
-        for (int i = 0; i < ejercito2.size() - 1; i++) {
-            for (int j = 0; j < ejercito2.size() - 1 - i; j++) {
-                if (ejercito2.get(j).getVida() < ejercito2.get(j + 1).getVida()) {
-                    // Intercambiar si está en el orden incorrecto
-                    Soldado temp = ejercito2.get(j);
-                    ejercito2.set(j, ejercito2.get(j + 1));
-                    ejercito2.set(j + 1, temp);
-                }
+    public static void rankingMayorMenorBurbuja(Soldado[] ej){
+        for (int i = 0; i < ej.length - 1; i++){
+            for(int j = 0; j < ej.length - i - 1; j++){
+                if(ej[j].getVida() < ej[j + 1].getVida()){
+                    Soldado aux = ej[j];
+                    ej[j] = ej[j + 1];
+                    ej[j + 1] = aux;
+                }   
             }
         }
     }
     
 
-
-    public static void rankingMayorMenorSeleccion(ArrayList<ArrayList<Soldado>> campo){
-        Soldado temp1, temp2;
-        // Recorre todas las posiciones del campoo
-        for (int i = 0; i < campo.size(); i++){
-            for (int j = 0; j < campo.get(i).size(); j++){
-                int filMenorEj1 = i, colMenorEj1 = j, filMenorEj2 = i, colMenorEj2 = j;
-
-                // Busca el soldado con la menor vida a partir de la posición actual
-                for (int k = i; k < campo.size(); k++){
-                    for (int l = 0; l < campo.get(k).size(); l++){
-                        
-                        // Evita revisar posiciones ya evaluadas
-                        if(campo.get(i).get(j) != null){
-                            if (campo.get(i).get(j).getNombre().charAt(campo.get(i).get(j).getNombre().length() - 1) == '1'){
-                               if (k == i && l <= j) {
-                                   continue;
-                               }
-                               // Cambia las coordenadas si se encuentra un soldado con vida más baja
-                               if (campo.get(k).get(l) != null && campo.get(filMenorEj1).get(colMenorEj1) != null){
-                                   if (campo.get(k).get(l).getVida() < campo.get(filMenorEj1).get(colMenorEj1).getVida()){
-                                       filMenorEj1 = k;
-                                       colMenorEj1 = l;
-                                   }
-                               }
-                           }
-                           if (campo.get(i).get(j).getNombre().charAt(campo.get(i).get(j).getNombre().length() - 1) == '2'){
-                               if (k == i && l <= j) {
-                                   continue;
-                               }
-                               // Cambia las coordenadas si se encuentra un soldado con vida más baja
-                               if (campo.get(k).get(l) != null && campo.get(filMenorEj2).get(colMenorEj2) != null){
-                                   if (campo.get(k).get(l).getVida() > campo.get(filMenorEj2).get(colMenorEj2).getVida()){
-                                       filMenorEj2 = k;
-                                       colMenorEj2 = l;
-                                   }
-                               }
-                           }
-                        }
-                        
-                    }
-                }
-                // Intercambia los soldados si se encontró uno con menor vida
-                if (filMenorEj1 != i || colMenorEj1 != j){
-                    temp1 = campo.get(i).get(j);
-                    campo.get(i).set(j, campo.get(filMenorEj1).get(colMenorEj1));
-                    campo.get(filMenorEj1).set(colMenorEj1, temp1);
-                }
-                if (filMenorEj2 != i || colMenorEj2 != j){
-                    temp2 = campo.get(i).get(j);
-                    campo.get(i).set(j, campo.get(filMenorEj2).get(colMenorEj2));
-                    campo.get(filMenorEj2).set(colMenorEj2, temp2);
-                }
+    public static void rankingMayorMenorSeleccion(Soldado[] ej){
+        for (int i = 0; i < ej.length - 1; i++){
+            int indexMayor = i;
+            for (int j = i + 1; j < ej.length; j++){
+                if(ej[j].getVida() > ej[indexMayor].getVida())
+                indexMayor = j;
             }
+            Soldado aux = ej[i];
+            ej[i] = ej[indexMayor];
+            ej[indexMayor] = aux;
         }
     }
 
     
-    public static void determinarGanador(ArrayList<ArrayList<Soldado>> campo){
-        double sumEj1 = 0, sumEj2 = 0;
-        // Recorre todo el campoo de soldados
-        for(int i = 0; i < campo.size(); i++){
-            for(int j = 0; j < campo.get(i).size(); j++){
-                // Si la posición actual contiene un soldado, suma su vida al total
-                if(campo.get(i).get(j) != null){
-                    if(campo.get(i).get(j).getNombre().charAt(campo.get(i).get(j).getNombre().length() - 1) == '1'){
-                        // Acumula el valor de la vida del soldado
-                        sumEj1 +=campo.get(i).get(j).getVida();
-                    }
-                    if(campo.get(i).get(j).getNombre().charAt(campo.get(i).get(j).getNombre().length() - 1) == '2'){
-                        // Acumula el valor de la vida del soldado
-                        sumEj2 +=campo.get(i).get(j).getVida();
-                    }
-                }
-                
-            }
+    public static void determinarGanador(Soldado[] ej1, Soldado[] ej2){
+        double sumLifeEj1 = 0, sumLifeEj2 = 0;
+        for(int i = 0; i < ej1.length; i++){
+            sumLifeEj1 += ej1[i].getVida();
+            sumLifeEj2 += ej2[i].getVida();
         }
-        if(sumEj1 == sumEj2){
-            System.out.println("\nEMPATE\nPuntos de vida totales: \nEjercito X1: " + sumEj1 +" = " + sumEj2 + " Ejercito X2");
+        if (sumLifeEj1 == sumLifeEj2) {
+            System.out.println("EMPATE\nEjercito1 = " + sumLifeEj1 + 
+            " ---- Ejercito2 = " + sumLifeEj2);
         }
-        else if(sumEj1 > sumEj2){
-            System.out.println("\nGANA EL EJERCITO X1\nPuntos de vida totales: \nEjercito X1: " + sumEj1 +" > " + sumEj2 + " Ejercito X2");
+        else if (sumLifeEj1 > sumLifeEj2) {
+            System.out.println("GANA EJERCITO1\nEjercito1 = " + sumLifeEj1 + 
+            "   >  Ejercito2 = " + sumLifeEj2);
         }
         else{
-            System.out.println("\nGANA EL EJERCITO X2\nPuntos de vida totales: \nEjercito X1: " + sumEj1 +" < " + sumEj2 + " Ejercito X2");
-        }
+            System.out.println("GANA EJERCITO2\nEjercito1 = " + sumLifeEj1 + 
+            "   <  Ejercito2 = " + sumLifeEj2);
+        }   
     }
 }
